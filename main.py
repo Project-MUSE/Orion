6613c18aa5735ee054dd6607156a39d492946e1a
Nicolas Gatien <nicolas.gatien6@gmail.com>
2023-08-06 12:11:35 -0400

    🤔 Opinion Generation

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import time
 import random
 from json import dumps
-from keybert import KeyBERT
 
 #########################
 #        UTILITY        #
@@ -47,25 +46,21 @@ def generate_initial_assistant_description(traits, goal, name):
     ]
     description = prompt_gpt(context)
     return description
-def generate_response(user_name, transcript):
+def generate_response(user_name, transcript, existing_opinions):
     context = [
         {
             "role": "system", 
             "content": f"""
 Here is the description of a character named Orion:
 [
-Orion is a stoic figure, exuding an aura that hints at countless untold stories of bravery and hardship. 
-Standing at over six feet tall, his broad shoulders carry the weight of responsibility with a graceful strength that belies the turbulence of his past. 
-His dark, almost obsidian eyes gleam with a wisdom gained from years of navigating treacherous terrains, both literal and metaphorical. 
-His hair, as black as a raven's wing, is perpetually tousled from his tendency to run his hands through it in moments of deep thought. 
-Orion's laugh, a rare and heartening sound, is like the rumbling of distant thunder, promising respite amidst the storms. 
-An intricate tattoo winds around his left forearm, an abstract tapestry that marries symbols of his heritage with tokens of personal triumph. 
-His enigmatic charm, coupled with an unwavering sense of justice, gives him an uncanny ability to inspire loyalty, making Orion a natural, though reluctant, leader. 
-He dresses in practical attire, with a preference for dark, earthy tones, making him look every bit the roguish explorer that his life's journey has shaped him to be.
+{CHARACTER_DESCRIPTION}
 ]
 
 Here is a transcript of a conversation between Orion and {user_name}:
 {transcript}
+
+Based on the following beliefs:
+{', '.join([d['opinion'] for d in existing_opinions])}
 
 How would Orion respond?
 Format your response as
@@ -76,21 +71,14 @@ Orion: <fill in>
 
     answer = prompt_gpt(context)
     return answer
-def personalize_generated_response(user_name, transcript, response):
+def personalize_generated_response(user_name, transcript, response, existing_opinions):
     context = [
         {
             "role": "system", 
             "content": f"""
 Here is the description of a character named Orion:
 [
-Orion is a stoic figure, exuding an aura that hints at countless untold stories of bravery and hardship. 
-Standing at over six feet tall, his broad shoulders carry the weight of responsibility with a graceful strength that belies the turbulence of his past. 
-His dark, almost obsidian eyes gleam with a wisdom gained from years of navigating treacherous terrains, both literal and metaphorical. 
-His hair, as black as a raven's wing, is perpetually tousled from his tendency to run his hands through it in moments of deep thought. 
-Orion's laugh, a rare and heartening sound, is like the rumbling of distant thunder, promising respite amidst the storms. 
-An intricate tattoo winds around his left forearm, an abstract tapestry that marries symbols of his heritage with tokens of personal triumph. 
-His enigmatic charm, coupled with an unwavering sense of justice, gives him an uncanny ability to inspire loyalty, making Orion a natural, though reluctant, leader. 
-He dresses in practical attire, with a preference for dark, earthy tones, making him look every bit the roguish explorer that his life's journey has shaped him to be.
+{CHARACTER_DESCRIPTION}
 ]
 
 Here is a transcript of a conversation between {user_name} and Orion:
@@ -99,7 +87,10 @@ Here is a transcript of a conversation between {user_name} and Orion:
 Here is a generated response for Orion:
 {response}
 
-Personalize the response further without making it longer, and turn it into something that is more accurate to what {name} would say to {user_name}
+Based on the following beliefs:
+{', '.join([d['opinion'] for d in existing_opinions])}
+
+Personalize the response further and limit it to a maximum of 3 sentences, and turn it into something that is more accurate to what {name} would say to {user_name}
 
 Format your response as:
 Orion: <fill in>
@@ -168,14 +159,7 @@ def end_conversation(user_name, transcript):
             "content": f"""
 Here is the description of a character named Orion:
 [
-Orion is a stoic figure, exuding an aura that hints at countless untold stories of bravery and hardship. 
-Standing at over six feet tall, his broad shoulders carry the weight of responsibility with a graceful strength that belies the turbulence of his past. 
-His dark, almost obsidian eyes gleam with a wisdom gained from years of navigating treacherous terrains, both literal and metaphorical. 
-His hair, as black as a raven's wing, is perpetually tousled from his tendency to run his hands through it in moments of deep thought. 
-Orion's laugh, a rare and heartening sound, is like the rumbling of distant thunder, promising respite amidst the storms. 
-An intricate tattoo winds around his left forearm, an abstract tapestry that marries symbols of his heritage with tokens of personal triumph. 
-His enigmatic charm, coupled with an unwavering sense of justice, gives him an uncanny ability to inspire loyalty, making Orion a natural, though reluctant, leader. 
-He dresses in practical attire, with a preference for dark, earthy tones, making him look every bit the roguish explorer that his life's journey has shaped him to be.
+{CHARACTER_DESCRIPTION}
 ]
 
 Here is a transcript of a conversation between {user_name} and Orion:
@@ -195,7 +179,7 @@ def generate_opinion_on_topic(topic):
         {
             "role": "system", 
             "content": f"""
-Suggest 10 distinct viewpoints/opinions about the following topic: {topic}
+Suggest 5 distinct viewpoints/opinions about the following topic: {topic}
 The viewpoints should be different enough that if two people with different viewpoints from the list were to talk, it could become a debate.
 Present each as equal in logical and ethical standing.          
 """
@@ -210,11 +194,35 @@ Present each as equal in logical and ethical standing.
     
     opinion = random.choice(topics)
     return opinion
+def get_most_recent_topic(transcript):
+    context = [
+        {"role": "system", "content": f"In 1 to 2 words, what is the most recent intellectual topic discussed in the following conversation transcript: {transcript}"}
+    ]
+    topic = prompt_gpt(context)
+    return topic 
+def get_any_new_opinions(transcript):
+    most_recent_topic = get_most_recent_topic(transcript)
+
+    for topic in existing_topics:
+        if topic["topic"]  == most_recent_topic:
+            return "", ""
+    
+    opinion = generate_opinion_on_topic(most_recent_topic)
+    existing_topics.append({"topic": most_recent_topic, "opinion": opinion})
+
+    return opinion, most_recent_topic
 
 MODEL = str("gpt-3.5-turbo")
 TEMPERATURE = float(0.7)
+CHARACTER_DESCRIPTION = """
+Orion Pax possesses an uncanny ability to light up any room he enters, his magnetic aura drawing people to him with an effortless charm. 
+His laughter is infectious, a melody that resonates deep within the hearts of those who hear it, making them feel as though they've known him for a lifetime. 
+With a gaze that seems to pierce the very essence of one's soul, Orion has an innate talent for understanding the desires and fears of those around him. 
+He wields his charisma not for personal gain, but to inspire and uplift, making every individual feel seen and valued. 
+Yet, beneath the vibrant facade, there's a depth to Orion that few get to see, a silent strength that anchors him amidst the storms of life.
+"""
 
-topic_extractor = KeyBERT('distilbert-base-nli-mean-tokens')
+existing_topics = []
 
 openai.api_key = get_api_key("openai")
 
@@ -228,17 +236,20 @@ while converse:
     question = input("\n" + user_name + ": ")
     transcript += f"\n{user_name}: {question}"
 
+    opinion, topic = get_any_new_opinions(transcript)
+
+    print(f"{divider()}\n {topic}: {opinion}")
+
     if len(transcript) > 10000:
         answer = end_conversation(user_name, transcript)
         converse = False
     else:
-        answer = generate_response(user_name, transcript)
+        answer = generate_response(user_name, transcript, existing_topics)
 
-    personalized_answer = personalize_generated_response(user_name, transcript, answer)
+    personalized_answer = personalize_generated_response(user_name, transcript, answer, existing_topics)
     transcript += f"\n{personalized_answer}"
 
     print("\n" + f"{personalized_answer}")
-    print(f"{divider()}\nTopics:\n{topic_extractor.extract_keywords(transcript)}")
    
     if user_name == "User":
         user_name = find_user_name(transcript, name)
